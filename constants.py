f4b55de1e3d32a4cf6d431601c43d90a183d80da
turbobeeper2005 <159623771+turbobeeper2005@users.noreply.github.com>
2024-02-10 13:53:44 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,39 @@
+contract_addr = {
+    "optimism":"0x32F05f390217990404392a4DdAF39D31Db4aFf77",
+    "celo":"0xad8676147360dBc010504aB69C7f1b1877109527",
+    "avalanche":"0x904550e0D182cd4aEe0D305891c666a212EC8F01",
+    "zkevm":"0x46B4eDaA761eF8d2934e9F7AAf32B5Bf2C9C9F67",
+    "bsc":"0x7b4f475d32f9c65de1834A578859F9823bE3c5Cf",
+    "moonbeam":"0xf3D41b377c93fA5C3b0071966f1811c5063fAD40",
+    "gnosis":"0xFD34afDFbaC1E47aFC539235420e4bE4A206f26D",
+    "arbitrum":"0xFD34afDFbaC1E47aFC539235420e4bE4A206f26D",
+    "polygon":"0x574E69C50e7D13B3d1B364BF0D48285A5aE2dF56",
+    "base":"0x5454cF5584939f7f884e95DBA33FECd6D40B8fE2",
+    "scroll":"0x904550e0D182cd4aEe0D305891c666a212EC8F01",
+    "ethereum":"0xf3D41b377c93fA5C3b0071966f1811c5063fAD40",
+}
+
+chain_info = {
+    'ethereum': {'rpc': 'https://rpc.ankr.com/eth', 'scan': 'https://etherscan.io/tx', 'token': 'ETH', 'chain_id': 1},
+    'optimism': {'rpc': 'https://rpc.ankr.com/optimism', 'scan': 'https://optimistic.etherscan.io/tx', 'token': 'ETH', 'chain_id': 10},
+    'bsc': {'rpc': 'https://rpc.ankr.com/bsc', 'scan': 'https://bscscan.com/tx', 'token': 'BNB', 'chain_id': 56},
+    'polygon': {'rpc': 'https://rpc.ankr.com/polygon', 'scan': 'https://polygonscan.com/tx', 'token': 'MATIC', 'chain_id': 137},
+    'zkevm': {'rpc': 'https://zkevm-rpc.com', 'scan': 'https://zkevm.polygonscan.com/tx', 'token': 'ETH', 'chain_id': 1101},
+    'arbitrum': {'rpc': 'https://rpc.ankr.com/arbitrum', 'scan': 'https://arbiscan.io/tx', 'token': 'ETH', 'chain_id': 42161},
+    'avalanche': {'rpc': 'https://rpc.ankr.com/avalanche', 'scan': 'https://snowtrace.io/tx', 'token': 'AVAX', 'chain_id': 43114},
+    'fantom': {'rpc': 'https://rpc.ankr.com/fantom', 'scan': 'https://ftmscan.com/tx', 'token': 'FTM', 'chain_id': 250},
+    'nova': {'rpc': 'https://nova.arbitrum.io/rpc', 'scan': 'https://nova.arbiscan.io/tx', 'token': 'ETH', 'chain_id': 42170},
+    'zksync': {'rpc': 'https://mainnet.era.zksync.io', 'scan': 'https://explorer.zksync.io/tx', 'token': 'ETH', 'chain_id': 324},
+    'celo': {'rpc': 'https://1rpc.io/celo', 'scan': 'https://celoscan.io/tx', 'token': 'CELO', 'chain_id': 42220},
+    'gnosis': {'rpc': 'https://rpc.ankr.com/gnosis', 'scan': 'https://gnosisscan.io/tx', 'token': 'xDAI', 'chain_id': 100},
+    'core': {'rpc': 'https://rpc.coredao.org', 'scan': 'https://scan.coredao.org/tx', 'token': 'CORE', 'chain_id': 1116},
+    'harmony': {'rpc': 'https://api.harmony.one', 'scan': 'https://explorer.harmony.one/tx', 'token': 'ONE', 'chain_id': 1666600000},
+    'moonbeam': {'rpc': 'https://rpc.ankr.com/moonbeam', 'scan': 'https://moonscan.io/tx', 'token': 'GLMR', 'chain_id': 1284},
+    'moonriver': {'rpc': 'https://moonriver.public.blastapi.io', 'scan': 'https://moonriver.moonscan.io/tx', 'token': 'MOVR', 'chain_id': 1285},
+    'linea': {'rpc': 'https://rpc.linea.build', 'scan': 'https://lineascan.build/tx', 'token': 'ETH', 'chain_id': 59144},
+    'base': {'rpc': 'https://mainnet.base.org', 'scan': 'https://basescan.org/tx', 'token': 'ETH', 'chain_id': 8453},
+    'zora': {'rpc': 'https://rpc.zora.energy', 'scan': 'https://zora.superscan.network/tx', 'token': 'ETH', 'chain_id': 7777777},
+    'scroll': {'rpc': 'https://scroll.blockpi.network/v1/rpc/public', 'scan': 'https://scrollscan.com/tx', 'token': 'ETH', 'chain_id': 534352}
+}
+
+bsc_gas = 1000000000
---
+++ main.py
@@ -0,0 +1,118 @@
+from web3 import Web3
+from abi import MERKLY_ABI
+import time
+import random
+from constants import chain_info, contract_addr, bsc_gas
+
+def get_base_gas():
+    try:
+        web3 = Web3(Web3.HTTPProvider(chain_info['ethereum']['rpc']))
+        gas_price = web3.eth.gas_price
+        gwei_gas_price = web3.from_wei(gas_price, 'gwei')
+        return gwei_gas_price
+    except Exception as error:
+        return get_base_gas()
+
+def wait_gas():
+    while True:
+        current_gas = get_base_gas()
+        if current_gas > MAX_GWEI:
+            print(f'current_gas : {current_gas} > {MAX_GWEI}')
+            time.sleep(60)
+        else:
+            break
+
+
+def mint_and_bridge(private_key: str, from_chain: str, to_chain: str):
+    from_rpc = chain_info[from_chain]['rpc']
+    to_id = chain_info[from_chain]['chain_id']
+
+    web3 = Web3(Web3.HTTPProvider(from_rpc))
+    pub_address = web3.eth.account.from_key(private_key).address
+    print(pub_address)
+    contract = web3.eth.contract(address=Web3.to_checksum_address(contract_addr[from_chain]), abi=MERKLY_ABI)
+    fee = contract.functions.fee().call()
+    amount_to_mint = random.randint(1, 5)
+    try:
+        wait_gas()
+        transaction = contract.functions.mint(pub_address, amount_to_mint).build_transaction({
+            'from': pub_address,
+            'gas': 0,
+            'gasPrice': 0,
+            'value': fee * amount_to_mint,
+            'nonce': web3.eth.get_transaction_count(pub_address),
+        })
+
+        transaction['gasPrice'] = bsc_gas if from_chain == 'bsc' else int(web3.eth.gas_price * random.uniform(1.01, 1.02))
+
+        pluser = [1.05, 1.07]
+        gasLimit = web3.eth.estimate_gas(transaction)
+        transaction['gas'] = int(gasLimit * random.uniform(pluser[0], pluser[1]))
+
+        signed_transaction = web3.eth.account.sign_transaction(transaction, private_key)
+        transaction_hash = web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+        tx_hash = web3.to_hex(transaction_hash)
+        print("Mint transaction hash:", tx_hash)
+
+        receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+        '''
+        if from_chain == 'Polygon':
+            mint_id_hex = receipt.logs[1].topics[3]
+        elif len(receipt.logs) == 2:
+            mint_id_hex = receipt.logs[1].topics[3]
+        else:
+            mint_id_hex = receipt.logs[0].topics[3]
+
+        mint_id = int.from_bytes(mint_id_hex, byteorder='big')
+        print("Mint ID:", mint_id)'''
+
+        quote = contract.functions.quoteBridge(to_id).call()
+
+        transaction = contract.functions.bridgeHFT(to_id, amount_to_mint * (10**18)).build_transaction({
+            'from': pub_address,
+            'gas': 0,
+            'gasPrice': 0,
+            'value': quote,
+            'nonce': web3.eth.get_transaction_count(pub_address),
+        })
+
+        transaction['gasPrice'] = bsc_gas if from_chain == 'bsc' else int(web3.eth.gas_price * random.uniform(1.01, 1.02))
+
+        pluser = [1.05, 1.07]
+        gasLimit = web3.eth.estimate_gas(transaction)
+        transaction['gas'] = int(gasLimit * random.uniform(pluser[0], pluser[1]))
+
+        signed_transaction = web3.eth.account.sign_transaction(transaction, private_key)
+        transaction_hash = web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+        tx_hash = web3.to_hex(transaction_hash)
+        print("Bridge transaction hash:", tx_hash)
+
+    except Exception as error:
+        print(error)
+
+if __name__ == '__main__':
+    '''
+    "optimism"
+    "celo"
+    "avalanche"
+    "zkevm"
+    "bsc"
+    "moonbeam"
+    "gnosis"
+    "arbitrum"
+    "polygon"
+    "base"
+    "scroll"
+    "ethereum"
+    '''
+    MAX_GWEI = 50
+    from_chain = ['base']
+    to_chain = ['celo']
+    with open('./wallets.txt', 'r') as file:
+        wallets = [line.strip() for line in file]
+
+    for wallet in wallets:
+        DELAY = random.randint(10, 15)
+        mint_and_bridge(wallet, random.choice(from_chain), random.choice(to_chain))
+        print(f'Sleep for {DELAY} seconds')
+        time.sleep(DELAY)
